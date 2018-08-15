4b5ebb26ce642572ab84cbb12e972ce2ec383448
Shaji Kalidasan <shajiindia@yahoo.com>
2018-08-15 19:51:12 +0530

    Create non_greedy_matching.pl

---
+++ non_greedy_matching.pl
@@ -0,0 +1,13 @@
+use 5.010;
+use strict;
+use warnings;
+
+#Greedy matching aka Lazy Matching
+while (<DATA>) {
+	if (/Betty (.*?) butter/) {
+		say $1;
+	}
+}
+
+__DATA__
+Betty bought some butter the butter was bitter so she bought some better butter to make the bitter butter better.
